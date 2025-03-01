663ba53de9cee0e24086bb40f44146083370c532
Santeri Hetekivi <santeri@hetekivi.com>
2025-03-01 17:29:51 +0200

    Default areas for extensions

--- modules/browser.nix
+++ modules/browser.nix
@@ -23,18 +23,22 @@
                 "jid1-ZAdIEUB7XOzOJw@jetpack" = {
                     install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
                     installation_mode = "force_installed";
+                    default_area = "menupanel";
                 };
                 "uBlock0@raymondhill.net" = {
                     install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
                     installation_mode = "force_installed";
+                    default_area = "menupanel";
                 };
                 "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
                     install_url = "https://addons.mozilla.org/firefox/downloads/file/4410896/bitwarden_password_manager-2024.12.4.xpi";
                     installation_mode = "force_installed";
+                    default_area = "navbar";
                 };
                 "sponsorBlocker@ajay.app" = {
                     install_url = "https://addons.mozilla.org/firefox/downloads/file/4424639/sponsorblock-5.11.5.xpi";
                     installation_mode = "force_installed";
+                    default_area = "menupanel";
                 };
             };
         };
