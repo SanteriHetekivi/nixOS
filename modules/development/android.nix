466da60dd661d3c87e28804b7911954d566fb3a2
Santeri Hetekivi <santeri@hetekivi.com>
2026-03-11 19:13:33 +0200

    ADB

--- modules/development/android.nix
+++ modules/development/android.nix
@@ -3,10 +3,11 @@
 
 {
   environment.systemPackages = with pkgs; [
+    android-tools
     android-studio-full
   ];
   nixpkgs.config.android_sdk.accept_license = true;
-  users.users.santeri.extraGroups = ["kvm"];
+  users.users.santeri.extraGroups = ["kvm" "adbusers"];
   system.stateVersion = "24.11";
 }
 
